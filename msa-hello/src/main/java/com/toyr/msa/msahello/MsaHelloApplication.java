9015b3ff8946f0111418ec9ae440594fe3d8c606
toyr <544810359@qq.com>
2020-05-09 12:49:14 +0800

    跨域

--- msa-hello/src/main/java/com/toyr/msa/msahello/MsaHelloApplication.java
+++ msa-hello/src/main/java/com/toyr/msa/msahello/MsaHelloApplication.java
@@ -2,11 +2,9 @@ package com.toyr.msa.msahello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @SpringBootApplication
 public class MsaHelloApplication {
